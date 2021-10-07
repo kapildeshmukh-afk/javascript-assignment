acc29b42483895e1ff3781b1c8bb30f83f42a412
kapildeshmukh-afk <80241134+kapildeshmukh-afk@users.noreply.github.com>
2021-10-07 13:31:38 +0530

    assignemnt day 3 javacript

---
+++ javascript day3/question1/script.js
@@ -0,0 +1,17 @@
+
+// using do-while loop
+const array1=[1,2,3,4,5];
+let count=0;
+do{
+    console.log(array1[count]);
+    count++;
+}while(count<=array1.length);
+
+// using while loop
+
+const array1=[1,2,3,4,5];
+let count=0;
+while( count<=array1.length){
+    console.log(array1[count]);
+    count++;
+}
